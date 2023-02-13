ad0def862aa207276fb762638b2ebc1bac837eb6
강승호 <kkang4913@naver.com>
2023-02-13 14:19:37 +0900

    git commit -m "docs: ignore, README 수정"

---
+++ src/test/java/hello/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
