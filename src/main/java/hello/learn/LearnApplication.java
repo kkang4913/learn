d9c5a468885f0a0f050b3bab426a58acc64d8adf
강승호 <kkang4913@naver.com>
2023-04-20 17:17:24 +0900

    서블릿 등록 사용

--- src/main/java/hello/learn/LearnApplication.java
+++ src/main/java/hello/learn/LearnApplication.java
@@ -2,7 +2,9 @@ package hello.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 스프링이 자동으로 현재 패키지 안에 하위 패키지를 찾아 자동으로 서블릿을 등록
 @SpringBootApplication
 public class LearnApplication {
 
