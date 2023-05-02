81ad0dcb61d4dbc93c482e47c4298d55bc6acfbf
강승호 <kkang4913@naver.com>
2023-05-02 17:22:45 +0900

    HTTP 요청 데이터 - API 메시지 바디 전송

--- src/main/java/hello/learn/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/learn/basic/request/RequestBodyStringServlet.java
@@ -15,11 +15,16 @@ import java.nio.charset.StandardCharsets;
 public class RequestBodyStringServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*
+         1.request.getInputStream(); -> 바이트 코드로 바로 얻을 수 있다.
+         2.바이트 코드를 string으로 변환 - > StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+         3.변환시 인코딩 정보를 알려주어야 한다.
+         */
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
         System.out.println("messageBody = " + messageBody);
 
-        response.getWriter().write("ok");
+        response.getWriter().write("message body ok");
     }
 }
